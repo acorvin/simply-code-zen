68731d7d8dd2d82cd936c17605326121de33006f
Alex Corvin <corvindesigns@gmail.com>
2023-08-18 14:16:19 -0600

    updating preview

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
         <nav>
             <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
                 <li><a class="hover:text-gray-200 hover:underline px-4" href="#">Get Merch</a></li>
-                <li><a class="hover:text-gray-200 hover:underline px-4" href="#">About Us</a></li>
+                <li><a class="hover:text-gray-200 hover:underline px-4" href="#">Join In</a></li>
             </ul>
         </nav>
 

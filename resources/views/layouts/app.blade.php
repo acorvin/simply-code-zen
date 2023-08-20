e906d32ec00770802c8e698d0799ad3224351600
Alex Corvin <corvindesigns@gmail.com>
2023-08-20 09:49:03 -0600

    removing welcome.blade, moving css styling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,14 +6,6 @@
     <title>{{ $metaTitle ?: 'Simply Code Zen' }}</title>
     <meta name="description" content="{{ $metaDescription }}">
 
-    <style>
-        @import url('https://fonts.googleapis.com/css?family=Karla:400,700&display=swap');
-
-        .font-family-karla {
-            font-family: karla;
-        }
-    </style>
-
     <!-- Font Awesome -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
     <!-- Vite -->

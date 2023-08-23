50a0ec8747de7662fa228c35833d9cfceb535212
Alex Corvin <corvindesigns@gmail.com>
2023-08-23 17:01:43 -0600

    adding view counts and count display widgets

--- app/Filament/Resources/PostResource/Pages/ViewPost.php
+++ app/Filament/Resources/PostResource/Pages/ViewPost.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\PostResource\Pages;
 
 use App\Filament\Resources\PostResource;
+use App\Filament\Widgets\PostOverview;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\ViewRecord;
 
@@ -14,6 +15,13 @@ class ViewPost extends ViewRecord
     {
         return [
             Actions\EditAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            PostOverview::class
         ];
     }
 }
---
+++ database/factories/PostViewFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PostView;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostViewFactory extends Factory
+{
+    protected $model = PostView::class;
+
+    public function definition(): array
+    {
+        return [
+
+        ];
+    }
+}

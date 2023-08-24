8bf8debe4adbc4160de83242138cf3e3c408d0a0
Alex Corvin <corvindesigns@gmail.com>
2023-08-24 17:17:34 -0600

    adding serachable and sortable functions

--- app/Filament/Resources/PostResource.php
+++ app/Filament/Resources/PostResource.php
@@ -73,17 +73,20 @@ class PostResource extends Resource
         return $table
             ->columns([
                 Tables\Columns\ImageColumn::make('thumbnail'),
-                Tables\Columns\TextColumn::make('title'),
+                Tables\Columns\TextColumn::make('title')->searchable(['title', 'body'] )->sortable(),
 //                Tables\Columns\TextColumn::make('slug'),
 //                Tables\Columns\TextColumn::make('body'),
                 Tables\Columns\IconColumn::make('active')
+                    ->sortable()
                     ->boolean(),
                 Tables\Columns\TextColumn::make('published_at')
+                    ->sortable()
                     ->dateTime(),
 //                Tables\Columns\TextColumn::make('user.name'),
 //                Tables\Columns\TextColumn::make('created_at')
 //                    ->dateTime(),
                 Tables\Columns\TextColumn::make('updated_at')
+                    ->sortable()
                     ->dateTime(),
             ])
             ->filters([

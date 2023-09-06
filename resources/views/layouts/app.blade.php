097388b3b22381200143202a40991c0ab8b6b63d
Alex Corvin <corvindesigns@gmail.com>
2023-09-06 06:57:53 -0600

    fixing auth in nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,11 +23,46 @@
         <nav>
             <ul class="flex items-center justify-between font-bold text-sm text-white uppercase no-underline">
                 @auth
-                <li><a href="/profile" class="hover:text-gray-200 hover:underline px-4">Profile</a></li>
-                <li><a href="#" class="hover:text-gray-200 hover:underline px-4" type="submit">Logout</a></li>
+                    <div class="flex sm:items-center sm:ml-6">
+                        <x-dropdown align="right" width="34">
+                            <x-slot name="trigger">
+                                <button
+                                    class="hover:bg-slate-800 hover:text-white flex items-center rounded py-2 px-4 mx-2">
+                                    <div>{{ Auth::user()->name }}</div>
+
+                                    <div class="ml-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                             viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd"
+                                                  d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                  clip-rule="evenodd"/>
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('profile.edit')">
+                                    {{ __('Profile') }}
+                                </x-dropdown-link>
+
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+
+                                    <x-dropdown-link :href="route('logout')"
+                                                     onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
+                            </x-slot>
+                        </x-dropdown>
+                    </div>
                 @else
-                <li><a href="{{ route('register') }}" class="hover:text-gray-200 hover:underline px-4">Sign Up!</a></li>
-                <li><a href="{{ route('login') }}" class="hover:text-gray-200 hover:underline px-4">Login</a></li>
+                    <a href="{{route('login')}}"
+                       class="hover:bg-slate-800 hover:text-white rounded py-2 px-4 mx-2">Login</a>
+                    <a href="{{route('register')}}" class="bg-slate-800 text-white rounded py-2 px-4 mx-2">Sign up!</a>
                 @endauth
             </ul>
         </nav>
